31c0804f82a66164232116411c5d1f7412ffd180
Keano-Booi <Keanobooi84@gmail.com>
2025-10-16 15:18:59 +0200

    changed my code

--- loops/App.tsx
+++ loops/App.tsx
@@ -1,12 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View,Button } from 'react-native';
+
 
 export default function App() {
+
+  const[output,setOutput] = useState<string[]>([]);
+  const numbers: number[]=[2,4,6,8,10];
+
+  const runFor=(): void=>{
+    const Lines: string[]=[];
+    for(let i =0;i<numbers.length;i++)
+    {Lines.push(`for: i=${i}->${numbers[i]}`)}
+    setOutput(Lines);
+  }
   return (
     <View style={styles.container}>
-      <Text>Loops</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Text style={styles.mainText}>🍑Loops</Text>
+      <View>
+     
+      <Button title="Start Loop" onPress={runFor}></Button>
+       {output.length===0 ?(
+        <Text>Output will always appear here</Text>
+      ) : (output.map((line,i) => <Text key={i}>{line}</Text>))
+
+      }
+      </View>
+
+      </View>
   );
 }
 
@@ -15,6 +35,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#7c2d2dff',
     alignItems: 'center',
-    justifyContent: 'center',
+    
+    
   },
+
+  mainText:{
+    fontSize:60,
+    fontStyle:'italic',
+
+  }
 });
